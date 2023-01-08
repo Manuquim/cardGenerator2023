49c75bf0963d2eb9cf7bfd33740edccc288105b3
Manuquim <manuelcabreraosorio11@gmail.com>
2023-01-08 20:33:07 +0000

    commit hecho el 080123

--- src/app.js
+++ src/app.js
@@ -2,10 +2,56 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Una vez carga la pagina, se genera una carta
+  generaCarta();
+
+  //añadimos evento sobre el div "card" para generar nueva carta
+  var el = document.getElementById("card");
+  el.onclick = generaCarta;
+
+  //genera carta cada 5 segundos
+  setInterval(generaCarta, 5000);
 };
+
+function generaCarta() {
+  document.getElementById("numero").innerHTML = nuevoNumero();
+
+  let palo;
+  document.getElementById("top").innerHTML = palo = nuevoPalo();
+  document.getElementById("bottom").innerHTML = palo;
+
+  let color = "black";
+  if (palo == "&hearts;" || palo == "&diams;") color = "red";
+  document.getElementById("top").style.color = color;
+  document.getElementById("bottom").style.color = color;
+}
+
+function nuevoPalo() {
+  let cuatroPalos = ["&spades;", "&hearts;", "&diams;", "&clubs;"];
+  return cuatroPalos[generaRandom(0, 3)];
+}
+function nuevoNumero() {
+  let numeroCarta = [
+    "As",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  return numeroCarta[generaRandom(0, 12)];
+}
+
+//genera numero aleatorio entero entre los dos valores de argumento
+function generaRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
